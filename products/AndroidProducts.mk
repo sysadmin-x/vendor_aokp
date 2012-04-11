2466ad0f0532301e939b9cb7541169e30ade4402
Mike Wilson <mwilson@acesappraisal.com>
2012-04-11 18:02:14 -0400

    G-tablet

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,4 +2,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/maguro.mk \
     $(LOCAL_DIR)/tenderloin.mk \
     $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/malata_smba1002.mk 
+    $(LOCAL_DIR)/smba1002.mk 
